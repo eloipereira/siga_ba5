ef0ddd3c1e7b1fd9c17452f223d74d9a8a844a32
Eloi Pereira <eloi@berkeley.edu>
2021-03-26 16:19:36 +0000

    layout e novos graficos de energia

---
+++ intro.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def intro():
+    st.write('bmabmambambma')
+    st.image('ba5brasao2712.png')
--- siga.py
+++ siga.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import pandas as pd
-import datetime
+import plotly.express as px
+from pathlib import Path
 import time
 import shelve
+from datetime import datetime
 from passlib.hash import sha256_crypt
+from geral import *
+from energia import *
+from intro import *
 
-def login(user,pw,db):
+
+def login(user,pw):
     if user in db:
-        if sha256_crypt.verify(db[user], pw):
+        if sha256_crypt.verify(pw,db[user]):
             db['logged'] = True
             return True
     return False
 
-def register(user,pw,db):
+def register(user,pw):
     db[user] = sha256_crypt.hash(pw)
     return True
 
@@ -21,48 +27,60 @@ def register(user,pw,db):
 st.title("SIGA-BA5")
 st.header("Sistema Integrado de Gestão Ambiental da BA5")
 
+user_input = st.empty()
+pw_input = st.empty()
+login_btn = st.empty()
+
 with shelve.open('auth') as db:
-    register('admin','admin',db)
-    #if 'logged' in db:
-    db['logged'] = False
+    register('admin','admin')
+    if 'logged' not in db:
+        db['logged'] = False
     if not db['logged']:
-        user = st.text_input('Username')
-        pw = st.text_input('Password',type='password')
-        if st.button('Login'):
-            logged_in = login(user,pw,db)
+        user = user_input.text_input('Username')
+        pw = pw_input.text_input('Password',type='password')
+        if login_btn.button('Login'):
+            logged_in = login(user,pw)
             if(logged_in):
                 db['current_user'] = user
                 st.success("Login successful")
             else:
                 st.error("Invalid credentials")
-    else:
-        st.subheader(f'Olá {user}')
+        else:
+            st.stop()
 
-area = st.sidebar.selectbox('Seleccionar área:', ['Geral', 'Energia', 'Pegada', 'Floresta', 'Financiamento', 'R&D'])
-data_init = st.sidebar.date_input('Data inicial')
-st.sidebar.date_input('Data final')
-if st.sidebar.checkbox('Configuração'):
-    st.sidebar.radio('Mostrar tabelas?', [True,False])
-    st.sidebar.radio('Mostrar gráficos?', [True,False])
+    user_input.empty()
+    pw_input.empty()
+    login_btn.empty()
 
-if area == 'Geral':
-    sliding = st.slider('select',0,100,step = 1)
-    st.write(data_init + datetime.timedelta(days=sliding))
-    st.select_slider('select me',[1,2,3])
-    st.time_input('my time')
-    st.spinner()
-    time.sleep(5)
+    current_user = db['current_user']
+    
+    area = st.sidebar.radio('Seleccionar área:', ['Geral', 'Energia', 'Emissões', 'Floresta', 'Financiamento', 'Info'])
 
+    today = datetime.today()
+    two_years_ago = datetime(today.year-2,today.month,today.day)
 
-if area == 'Energia':
-    st.empty()
+    data_init = st.sidebar.date_input('Data inicial',value=two_years_ago)
+    data_end = st.sidebar.date_input('Data final',value=today)
 
-if area == 'Pegada':
-    st.empty()
+    st.subheader(f'Olá {current_user}!')
 
-if area == 'Financiamento':
-    st.empty()
+    with st.sidebar.beta_expander('Configuration'):
+        is_table = st.checkbox('Mostrar tabelas?', value=False)
+        is_plot = st.checkbox('Mostrar gráficos?', value=True)
 
-if area == 'R&D':
-    st.empty()
+    if area == 'Geral':
+        geral(data_init,data_end,is_table,is_plot)
+
+    if area == 'Energia':
+        energy_global_pie(data_init,data_end,is_table,is_plot)
+        energy_global(data_init,data_end,is_table,is_plot)
+
+    if area == 'Emissões':
+        st.empty()
+
+    if area == 'Financiamento':
+        st.empty()
+
+    if area == 'Info':
+        intro()
 
