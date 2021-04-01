81ed47c873e63991c5978335d03334e98245dc5c
Eloi Pereira <eloi@berkeley.edu>
2021-04-01 23:14:58 +0100

    change layout

--- energia.py
+++ energia.py
@@ -8,14 +8,15 @@ def load_energy_data(data_init,data_end):
     df = pd.read_csv('energy_data.csv', index_col=0, parse_dates=True,dtype='float64')
     df = df.loc[data_init:data_end]
     df['renewal_energy_purchased'].fillna(df['electric_energy_purchased']*renewal_ratio_nan,inplace=True)
-    df['Não-renovável fornecida/kWh'] = df['electric_energy_purchased'] - df['renewal_energy_purchased']
-    df = df.drop(columns = ['electric_energy_purchased'])
-    df = df.rename(columns = {
-        'renewal_energy_purchased': 'Renovável fornecida/kWh',
-        'renewal_energy_produced': 'Renovável produzida/kWh'
-    })
+    df['Não-renovável fornecida'] = df['electric_energy_purchased'] - df['renewal_energy_purchased']
+    df.drop(columns = ['electric_energy_purchased'], inplace=True)
+    df.index.name = 'Tempo/mês'
+    df.rename(
+        columns = {
+            'renewal_energy_purchased': 'Renovável fornecida',
+            'renewal_energy_produced': 'Renovável produzida'
+        }, inplace=True)
     return df
-
 
 def energy_global_pie(data_init,data_end,is_table=False,is_plot=True):
     df = load_energy_data(data_init,data_end)
@@ -23,9 +24,24 @@ def energy_global_pie(data_init,data_end,is_table=False,is_plot=True):
     column_name = 'Energia/kWh'
     title = f'Balanço energetico de {data_init} a {data_end}'
     df_sum[column_name] = df.sum()
+    df_sum.index.name = 'Variável'
     if (df.size > 0): 
         if is_plot:
-            fig = px.pie(df_sum,values=column_name,names=df_sum.index,title=title,color=df_sum.index)
+            fig = px.pie(df_sum,
+                values=column_name,
+                names=df_sum.index,
+                title=title,
+                color=df_sum.index)
+            fig.update_layout(
+                legend=dict(
+                    orientation="h",
+                    yanchor="top",
+                    xanchor="center",
+                    x = 0.5,
+                    y=-0.1,
+                    itemwidth=40
+                )
+            )
             st.plotly_chart(fig)
         if is_table:
             st.dataframe(df_sum.style.format("{:.2f}"))
@@ -37,7 +53,22 @@ def energy_global(data_init,data_end,is_table,is_plot):
     title = f'Balanço energetico de {data_init} a {data_end}'
     if (df.size > 0):
         if is_plot:
-            fig = px.area(df,title=title)
+            fig = px.area(df,
+                title=title)
+            fig.update_yaxes(
+                title_text = "Energia/kWh" 
+            )
+            fig.update_layout(
+                legend_title = '',
+                legend=dict(
+                    orientation="h",
+                    yanchor="top",
+                    xanchor="center",
+                    x = 0.5,
+                    y=-0.2,
+                    itemwidth=40
+                )
+            )
             st.plotly_chart(fig)
         if is_table:
             st.dataframe(df.style.format("{:.2f}"))
