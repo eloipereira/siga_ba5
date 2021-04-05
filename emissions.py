7a4cfad87df65bb6f72b26635dbe0cdbc72c16d4
FilipeMSDelgado <80927690+FilipeMSDelgado@users.noreply.github.com>
2021-04-05 14:59:34 +0100

    Update emissions.py

--- emissions.py
+++ emissions.py
@@ -13,42 +13,6 @@ def load_data(data_init,data_end):
         }, inplace=True)
     return df
 
-def total_pie(data_init,data_end,is_table=False,is_plot=True):
-    df = load_data(data_init,data_end)
-    df_sum = pd.DataFrame()
-    column_name = 'GEE/tonCO2eq'
-    title = f'Emissões de GEE de {data_init} a {data_end}'
-    df_sum[column_name] = df.sum()
-    df_sum.index.name = 'Variável'
-    if (df.size > 0): 
-        if is_plot:
-            fig = px.pie(df_sum,
-                values=column_name,
-                names=df_sum.index,
-                color=df_sum.index
-            )
-            fig.update_layout(
-                title=dict(
-                    text=title,
-                    xanchor="center",
-                    yanchor="top",
-                    x=0.5,
-                    y=0.93
-                ),
-                legend=dict(
-                    orientation="h",
-                    yanchor="top",
-                    xanchor="center",
-                    x = 0.5,
-                    y=-0.1,
-                    itemwidth=40
-                )
-            )
-            st.plotly_chart(fig)
-        if is_table:
-            st.dataframe(df_sum.style.format("{:.2f}"))
-    else: 
-        st.write("Periodo sem dados.")
 
 def time_series(data_init,data_end,is_table,is_plot):
     df = load_data(data_init,data_end)
@@ -57,7 +21,7 @@ def time_series(data_init,data_end,is_table,is_plot):
         if is_plot:
             fig = px.area(df)
             fig.update_yaxes(
-                title_text = "Energia/kWh" 
+                title_text = "GEE/tonCO2eq" 
             )
             fig.update_layout(
                 title=dict(
